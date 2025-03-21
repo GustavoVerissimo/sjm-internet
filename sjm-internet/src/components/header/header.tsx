cf1c0eabc5f500c0039c974e9d772afda0a056d0
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-03-21 14:44:18 -0300

    atualizacoes na estrutura da header

--- sjm-internet/src/components/header/header.tsx
+++ sjm-internet/src/components/header/header.tsx
@@ -3,21 +3,23 @@ import sjmLogo from "../../../public/image/sjmLogo.webp"
 
 export default function Header() {
     return (
-      <header className="bg-(--color-blue)">
-        <div className="flex flex-row items-center py-[15px] px-[20px] w-[1200px] h-[100px]">
-          <div className="py">
-            <Image src={sjmLogo} alt="Imagem que contém a logo da empresa SJM INTERNET" width='200' height='73.5' />
+      <header>
+        <div className="flex flex-row items-center justify-around py-[15px] px-[20px] text-white bg-(--color-background-blue)">
+          <div>
+            <Image src={sjmLogo} alt="Imagem que contém a logo da empresa SJM INTERNET" width='150' height='73.5' />
           </div>
-          
-          <div className="color-white">
+          <div>
             <ul className="flex flex-row pr-4">
               <li className="pr-4"><a href="/">Início</a></li>
               <li className="pr-4"><a href="/">Sobre</a></li>
-              <li className="pr-4"><a href="/">Preço</a></li>
+              <li className="pr-4"><a href="/">Preços</a></li>
               <li className="pr-4"><a href="/">Promoções</a></li>
               <li className="pr-4"><a href="/">Contato</a></li>
             </ul>
           </div>
+        </div>
+        <div className="bg-(--color-background-grey) w-full h-[300px]">
+            <p className="text-white">ideias</p>
         </div>
       </header>
     ) 
