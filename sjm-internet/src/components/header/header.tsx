639831ff57505cf8bd8b2dc191dda95e95d138d4
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-03-25 16:34:57 -0300

    adicionando responsividade no header

--- sjm-internet/src/components/header/header.tsx
+++ sjm-internet/src/components/header/header.tsx
@@ -5,8 +5,8 @@ import Button from "../button/button"
 
 export default function Header() {
     return (
-      <header id="Inicio">
-        <div className="flex flex-row items-center justify-around py-[15px] px-[20px] text-white bg-(--color-background-blue)">
+      <header id="Inicio" className="min-w-[320px]  h-[auto]">
+        <div className="flex flex-row max-sm:flex-col items-center sm:justify-around py-[15px] px-[20px] text-white bg-(--color-background-blue)">
           <div>
             <Image src={sjmLogo} alt="Imagem que contém a logo da empresa SJM INTERNET" width='150' height='73.5' />
           </div>
@@ -20,14 +20,14 @@ export default function Header() {
             </ul>
           </div>
         </div>
-        <div className="flex flex-row items-center justify-between max-w-[100%] h-full font-(--font-display-rubik)">
+        <div className="flex flex-row max-sm:flex-col-reverse items-center justify-between font-(--font-display-rubik)">
           <div className="p-[30px]">
-            <h1 className="text-(--color-text-blue) text-[70px] font-bold ">Conecte-se ao Futuro</h1>
-            <p className="text-(--color-text-grey) text-[20px] mb-2">Fibra ótica de alta velocidade para sua casa e empresa</p>
+            <h1 className="font-bold text-(--color-text-blue) 3xl:text-[70px] xl:text-[60px] lg:text-[50px] md:text-[40px] max-sm:text-[28px]">Conecte-se ao Futuro</h1>
+            <p className="mb-2 text-(--color-text-grey) 3xl:text-[20px] xl:text-[15px] text-[15px] ">Fibra ótica de alta velocidade para sua casa e empresa</p>
             <Button titleButton="Confira Nossos Planos" />
           </div>
-            <Image src={Banner} alt="imagem que contém os planos de internet da empresa" className="2xl:w-[1000px] 2xl:h-[500px] rounded-bl-3xl " />
-          </div>
+            <Image src={Banner} alt="imagem que contém os planos de internet da empresa" className="2xl:w-[1000px] 2xl:h-[500px] xl:w-[800px] xl:h-[350px] lg:w-[600px] lg:h-[300px] md:w-[400px] md:h-[300px]  rounded-bl-3xl " />
+        </div>
       </header>
     ) 
   }
