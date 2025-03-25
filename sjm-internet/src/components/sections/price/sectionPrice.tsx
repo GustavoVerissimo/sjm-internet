38d008411aace1fe6fe39051e6dd7352277277fa
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-03-25 16:35:37 -0300

    iniciando estrutura da section de planos e precos

--- sjm-internet/src/components/sections/price/sectionPrice.tsx
+++ sjm-internet/src/components/sections/price/sectionPrice.tsx
@@ -1,7 +1,26 @@
 export default function sectionPrice(){
     return(
-        <section className="bg-(--color-background-grey) 2xl:h-[1000px] ">
-            <div>Olá Mundo</div>
+        <section className="flex flex-col items-center justify-center p-[20px] bg-(--color-background-grey) 2xl:h-[1000px]">
+            <h2 className="text-[--color-text-blue] text-[30px]">Planos e Preços</h2>
+            <div className="mt-10 justify-items-center">
+                <h3>Fibra ótica</h3>
+                <div className="flex flex-row max-sm:flex-col">
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[200px] h-[300px] rounded-xl"></div>
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[200px] h-[300px] rounded-xl"></div>
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[200px] h-[300px] rounded-xl"></div>
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[200px] h-[300px] rounded-xl"></div>
+                </div>
+            </div>
+            <div className=" mt-10 justify-items-center">
+                <h3>Fibra + Streaming</h3>
+                <div className="flex flex-row max-sm:flex-col">
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[200px] h-[300px] rounded-xl"></div>
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[200px] h-[300px] rounded-xl"></div>
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[200px] h-[300px] rounded-xl"></div>
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[200px] h-[300px] rounded-xl"></div>
+                </div>
+            </div>
+
         </section>
     )
 }
