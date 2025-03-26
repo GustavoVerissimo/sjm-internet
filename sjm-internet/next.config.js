30a89b70b738f64ee65b50e36d5925d65f5a9483
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-03-26 16:10:37 -0300

    adicionando informações da seçao de precos e planos

--- sjm-internet/next.config.js
+++ sjm-internet/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'https://drive.google.com/file/d/1ELEL49hU-rVtPiKcSfOWMHIJ472HHnOv/view?usp=drive_link'
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
