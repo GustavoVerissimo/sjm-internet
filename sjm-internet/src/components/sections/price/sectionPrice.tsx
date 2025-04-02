75a3a709f377020cc60ee626eef5473259fbf57e
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-04-02 00:03:35 -0300

    correcoes na section price

--- sjm-internet/src/components/sections/price/sectionPrice.tsx
+++ sjm-internet/src/components/sections/price/sectionPrice.tsx
@@ -8,21 +8,18 @@ export default function Price(){
             <h2 className="text-(--color-text-blue) font-bold text-[50px]">Fibra</h2>
             <div className="mt-10 justify-items-center">
                 <div className="flex flex-row max-sm:flex-col">
-                    <div className="mt-[20px] sm:mr-5 bg-white w-[250px] h-[500px] max-sm:w-[200px] max-sm:h-[300px] rounded-xl">
-    
+                    <div className="mt-[20px] sm:mr-5 bg-white w-[350px] h-[350px] max-sm:w-[200px] max-sm:h-[300px] rounded-xl justify-items-center">
+                        <div className=" w-full justify-items-center items-center border-b">
+                            <p className="text-[50px] text-(--color-text-blue2) font-black">100</p>
+                            <p className="text-[50px] text-(--color-text-blue2) font-black">MEGA</p>
+                            <p className="mb-2">Mais Velocidade, Menos Preço</p>
+                        </div>
+                        <div className="mt-2">
+                            <p className="flex flex-row text-(--color-text-grey)">R$<p className="text-[30px]"><strong>75</strong></p>,90/mês</p>
+                            <p className="text-[10px]">Pagando até o vencimento</p>
+                            <p className="flex flex-row text-(--color-text-grey)">R$<p className="text-[30px]"><strong>65</strong></p>,90/mês</p>
+                        </div>
                         <Button titleButton="Assine já" />
-                    </div>
-                    <div className="mt-[20px] sm:mr-5 bg-white w-[250px] h-[500px] max-sm:w-[200px] max-sm:h-[300px] rounded-xl">
-    
-                        <Button titleButton="Assine já" />
-                    </div>
-                    <div className="mt-[20px] sm:mr-5 bg-white w-[250px] h-[500px] max-sm:w-[200px] max-sm:h-[300px] rounded-xl">
-    
-                        <Button titleButton="Assine já" />
-                    </div>
-                    <div className="mt-[20px] sm:mr-5 bg-white w-[250px] h-[350px] max-sm:w-[200px] max-sm:h-[300px] rounded-xl">
-    
-                        <Button titleButton="Assine já"/>
                     </div>
                 </div>
             </div>       
