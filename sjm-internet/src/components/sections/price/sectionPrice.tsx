242b2842954341e63f611f13a15f93d44fc014ea
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-04-14 16:30:29 -0300

    terminando section streaming e iniciando sections question

--- sjm-internet/src/components/sections/price/sectionPrice.tsx
+++ sjm-internet/src/components/sections/price/sectionPrice.tsx
@@ -5,8 +5,8 @@ import Image from "next/image"
 export default function Price(){
 
     return(
-        <section className="flex flex-col p-3 items-center justify-center bg-(--color-background-grey)">
-            <div className="justify-items-center">
+        <section className="flex flex-col px-3 items-center justify-center bg-(--color-background-grey)">
+            <div className="justify-items-center  mb-[20px]">
                 <div className="flex flex-row max-sm:flex-col max-lg:flex-wrap max-xl:flex-wrap justify-center">
                     <div className="mt-[20px] md:mr-[30px] p-5 sm:mx-1 bg-white w-[350px] h-[390px] lg:w-[290px] lg:h-[370px] max-sm:w-[250px] max-sm:h-[300px] rounded-xl justify-items-center shadow-2xl">
                         <div className=" w-full justify-items-center items-center border-b">
@@ -21,62 +21,62 @@ export default function Price(){
                         </div>
                         <button className="w-64 max-sm:w-40 bg-(--color-button-blue) hover:bg-(--color-button-darkblue) sm:hover:w-68 text-white px-3 py-2 rounded-3xl">Assine Já</button>
                     </div>
-                    <div className="mt-[20px] md:mr-[30px] p-5 sm:mx-1 bg-(--color-background-blue) w-[350px] h-[390px] lg:w-[290px] lg:h-[370px] max-sm:w-[250px] max-sm:h-[300px] rounded-xl justify-items-center shadow-2xl">
-                        <div className=" w-full justify-items-center items-center border-b border-white">
-                            <p className="text-[50px] max-sm:text-[35px] text-white font-black">450</p>
-                            <p className="text-[50px] max-sm:text-[35px] text-white font-black">MEGA</p>
-                            <p className="mb-2 max-sm:text-[10px] text-white">Mais Velocidade, Menos Preço</p>
+                    <div className="mt-[20px] md:mr-[30px] p-5 sm:mx-1 bg-white w-[350px] h-[390px] lg:w-[290px] lg:h-[370px] max-sm:w-[250px] max-sm:h-[300px] rounded-xl justify-items-center shadow-2xl">
+                        <div className=" w-full justify-items-center items-center border-b">
+                            <p className="text-[50px] max-sm:text-[35px] text-(--color-text-blue) font-black">450</p>
+                            <p className="text-[50px] max-sm:text-[35px] text-(--color-text-blue) font-black">MEGA</p>
+                            <p className="mb-2 max-sm:text-[10px] text-(--color-text-grey)">Mais Velocidade, Menos Preço</p>
                         </div>
                         <div className="mt-2 justify-items-center">
                             <div className="flex flex-row items-center justify-around">
-                                <div className="bg-(--color-background-grey) rounded-xl mr-1">
+                                <div className="mr-1">
                                     <Image src={watchImagePrice} alt="logo watch" className="p-1"/>
                                 </div>
-                                <div className="bg-(--color-background-grey) rounded-xl">
+                                <div className="">
                                     <Image src={paramountImagePrice} alt="logo paramount" className="p-1"/>
                                 </div>
                             </div>
-                            <p className="mt-2 mb-1 flex flex-row text-white">R$<p className="text-[30px] max-sm:text-[20px]"><strong>104</strong></p>,99/mês</p>
+                            <p className="mt-2 mb-1 flex flex-row text-(--color-text-grey)">R$<p className="text-[30px] max-sm:text-[20px]"><strong>104</strong></p>,99/mês</p>
                         </div>
-                        <button className="w-64 max-sm:w-40 bg-white hover:bg-(--color-button-darkblue) hover:text-white sm:hover:w-68 text-(--color-background-blue) px-3 py-2 rounded-3xl">Assine Já</button>
+                        <button className="w-64 max-sm:w-40 bg-(--color-button-blue) hover:bg-(--color-button-darkblue) sm:hover:w-68 text-white px-3 py-2 rounded-3xl">Assine Já</button>
                     </div>
                     <div className="mt-[20px] md:mr-[30px] p-5 sm:mx-1 bg-white w-[350px] h-[390px] lg:w-[290px] lg:h-[370px] max-sm:w-[250px] max-sm:h-[300px] rounded-xl justify-items-center shadow-2xl">
                         <div className=" w-full justify-items-center items-center border-b">
                             <p className="text-[50px] max-sm:text-[35px] text-(--color-text-blue) font-black">650</p>
                             <p className="text-[50px] max-sm:text-[35px] text-(--color-text-blue) font-black">MEGA</p>
-                            <p className="mb-2 max-sm:text-[10px]">Mais Velocidade, Menos Preço</p>
+                            <p className="mb-2 max-sm:text-[10px] text-(--color-text-grey)">Mais Velocidade, Menos Preço</p>
                         </div>
                         <div className="mt-2 justify-items-center">
                             <div className="flex flex-row items-center justify-around">
-                                <div className="bg-(--color-background-grey) rounded-xl mr-1">
+                                <div className="mr-1">
                                     <Image src={watchImagePrice} alt="logo watch" className="p-1"/>
                                 </div>
-                                <div className="bg-(--color-background-grey) rounded-xl">
+                                <div className="">
                                     <Image src={paramountImagePrice} alt="logo paramount" className="p-1"/>
                                 </div>
                             </div>
-                            <p className="mt-2 mb-1 flex flex-row">R$<p className="text-[30px] max-sm:text-[20px]"><strong>119</strong></p>,99/mês</p>
+                            <p className="mt-2 mb-1 flex flex-row text-(--color-text-grey)">R$<p className="text-[30px] max-sm:text-[20px]"><strong>119</strong></p>,99/mês</p>
                         </div>
                         <button className="w-64 max-sm:w-40 bg-(--color-button-blue) hover:bg-(--color-button-darkblue) sm:hover:w-68 text-white px-3 py-2 rounded-3xl">Assine Já</button>
                     </div>
-                    <div className="mt-[20px] mr-1 p-5 sm:mx-1 bg-(--color-background-blue) w-[350px] h-[390px] lg:w-[290px] lg:h-[370px] max-sm:w-[250px] max-sm:h-[300px] rounded-xl justify-items-center shadow-2xl">
-                        <div className=" w-full justify-items-center items-center border-b border-white">
-                            <p className="text-[50px] max-sm:text-[35px] text-white font-black">800</p>
-                            <p className="text-[50px] max-sm:text-[35px] text-white font-black">MEGA</p>
-                            <p className="mb-2 max-sm:text-[10px] text-white">Mais Velocidade, Menos Preço</p>
+                    <div className="mt-[20px] mr-1 p-5 sm:mx-1 bg-white w-[350px] h-[390px] lg:w-[290px] lg:h-[370px] max-sm:w-[250px] max-sm:h-[300px] rounded-xl justify-items-center shadow-2xl">
+                        <div className=" w-full justify-items-center items-center border-b">
+                            <p className="text-[50px] max-sm:text-[35px] text-(--color-text-blue) font-black">800</p>
+                            <p className="text-[50px] max-sm:text-[35px] text-(--color-text-blue) font-black">MEGA</p>
+                            <p className="mb-2 max-sm:text-[10px] text-(--color-text-grey)">Mais Velocidade, Menos Preço</p>
                         </div>
                         <div className="mt-2 justify-items-center">
                             <div className="flex flex-row items-center justify-around">
-                                <div className="bg-(--color-background-grey) rounded-xl mr-1">
+                                <div className="mr-1">
                                     <Image src={watchImagePrice} alt="logo watch" className="p-1"/>
                                 </div>
-                                <div className="bg-(--color-background-grey) rounded-xl">
+                                <div className="">
                                     <Image src={paramountImagePrice} alt="logo paramount" className="p-1"/>
                                 </div>
                             </div>
-                            <p className="mt-2 mb-1 flex flex-row text-white">R$<p className="text-[30px] max-sm:text-[20px]"><strong>129</strong></p>,99/mês</p>
+                            <p className="mt-2 mb-1 flex flex-row text-(--color-text-grey)">R$<p className="text-[30px] max-sm:text-[20px]"><strong>129</strong></p>,99/mês</p>
                         </div>
-                        <button className="w-64 max-sm:w-40 bg-white hover:bg-(--color-button-darkblue) hover:text-white sm:hover:w-68 text-(--color-background-blue) px-3 py-2 rounded-3xl">Assine Já</button>
+                        <button className="w-64 max-sm:w-40 bg-(--color-button-blue) hover:bg-(--color-button-darkblue) sm:hover:w-68 text-white px-3 py-2 rounded-3xl">Assine Já</button>
                     </div>      
                 </div>
             </div>       
--- sjm-internet/src/components/sections/streaming/sectionStreaming.tsx
+++ sjm-internet/src/components/sections/streaming/sectionStreaming.tsx
@@ -8,22 +8,22 @@ import LogoWatch from "../../../../public/image/logo-watch-branca.svg"
 
 export default function Streaming() {
     return (
-        <section className="items-center justify-center"> 
-            <div className="flex flex-col mt-2 items-center text-center">
-                <div className="w-[100%] h-[100%] bg-(--color-background-darkblue) justify-around">
+        <section className="items-center justify-center bg-(--color-background-darkblue)"> 
+            <div className="flex flex-col  items-center text-center">
+                <div className="w-[100%] h-[100%] my-10 bg-(--color-background-darkblue) justify-around">
                     <div className="flex flex-row max-sm:flex-col-reverse m-3 items-center justify-around">
                         <div className="max-w-[500px] max-h-[500px]">
-                            <h2 className="mt-1 text-white font-bold text-center text-[40px] max-sm:text-[30px]">Fibra + Watch Streaming</h2>
-                            <p className="text-[20px] max-sm:text-[18px] text-left text-white">Fibra Ótica de alta velocidade para assistir a filmes e series no seu Streaming Favorito</p>
+                            <h2 className="my-5 text-white font-bold text-center text-[40px] max-sm:text-[30px]">Fibra + Watch</h2>
+                            <p className="text-[20px] max-sm:text-[18px] max-sm:ml-2 text-left text-white">Fibra Ótica de alta velocidade para assistir a filmes e series no seu Streaming Favorito</p>
                             <div className="flex flex-row my-5 justify-center">
                                 <Image src={LogoWatch} alt="logoWatch" className="w-[80px] h-[25px] md:w-[130px] md:h-[45px]"/>
                                 <Image src={logoParamountBranca} alt="logoParamount" className="w-[80px] h-[30px] md:w-[130px] md:h-[60px]"/>
                                 <Image src={logoMaxBranca} alt="logoMax" className="w-[80px] h-[30px] md:w-[130px] md:h-[60px]"/>
                                 <Image src={logoTelecineBranca} alt="logoTelecine" className="w-[80px] h-[30px] md:w-[130px] md:h-[60px]"/>
                             </div>
-                            <p className="text-[20px] max-sm:text-[18px] text-left text-white"> Disponiveis nos planos:</p>
+                            <p className="text-[20px] max-sm:text-[18px] max-sm:ml-2 text-left text-white"> Disponiveis nos planos:</p>
                             <p className="text-[20px] font-black max-sm:text-[18px] text-left max-sm:pl-2 text-white">450, 650 e 800 MEGA</p>
-                            <div className="flex flex-col items-center justify-center">
+                            <div className="flex flex-col max-sm:items-center max-sm:justify-center">
                                 <button className="mt-3 w-64 bg-white hover:bg-(--color-button-blue) sm:hover:w-68 text-(--color-text-blue) px-3 py-2 rounded-3xl">Ligue</button>
                                 <button className="mt-3 w-64 bg-(--color-button-gren) sm:hover:w-68 text-white px-3 py-2 rounded-3xl">WhatsApp</button>
                             </div>                            
