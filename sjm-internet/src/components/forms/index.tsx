93268f8b87d54d1c90460679a9b76c8e2c6a2dc9
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-04-18 01:06:29 -0300

    ajustes formulario

--- sjm-internet/src/components/forms/index.tsx
+++ sjm-internet/src/components/forms/index.tsx
@@ -10,80 +10,67 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
 import { Input } from "../ui/input"
 
-const formSchema = z.object({
-    seunome: z.string().min(2, {
-      message: "Seu nome precisa ter no mínimo 2 caracteres.",
-    }),
-    seuemail: z.string().min(2, {
-        message: "Seu email precisa ter no mínimo 2 caracteres.",
-      }),
-    seunumero: z.number().min(11, {
-        message: "Seu número precisa ter no mínimo 11 números",
-    }),
-  })
-
-  function OnSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+type TformSchema = {
+    nome: string
+    email: string
+    numero: string
   }
 
 export default function FormSectionQuestion(){
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<TformSchema>({
         defaultValues: {
-          seunome: "",
-          seuemail: "",
-          seunumero: 0,
-        },
+          nome: "",
+          email: "",
+          numero: ""
+        }
       })
+
+      function handleMensageForm(data: TformSchema){
+        console.log(data)
+      }
 
     return (
         <Card>
             <CardContent>
                 <Form {...form}>
-                    <form className="space-y-8">
+                    <form className="space-y-8" onSubmit={form.handleSubmit(handleMensageForm)}>
                         <FormField 
                             control={form.control} 
-                            name="seunome"
-                            render={(field) => (
+                            name="nome"
+                            render={({ field }) => (
                                 <FormItem>
                                     <FormLabel>Seu Nome</FormLabel>
                                     <FormControl>
-                                        <Input placeholder="Ex: Gustavo" {...field}/>
+                                        <Input placeholder="Ex: Gustavo" {...field} />
                                     </FormControl>
-                                    <FormDescription />
-                                    <FormMessage />
                                 </FormItem>
                             )}
                         />
                         <FormField 
                             control={form.control} 
-                            name="seuemail"
-                            render={(field) => (
+                            name="email"
+                            render={({ field }) => (
                                 <FormItem>
                                     <FormLabel>Seu Email</FormLabel>
                                     <FormControl>
-                                        <Input placeholder="Ex: Gustavo@gmail.com" {...field}/>
+                                        <Input placeholder="Ex: Gustavo@gmail.com" {...field} type="email" />
                                     </FormControl>
-                                    <FormDescription />
-                                    <FormMessage />
                                 </FormItem>
                             )}
                         />
                         <FormField 
                             control={form.control} 
-                            name="seunumero"
-                            render={(field) => (
+                            name="numero"
+                            render={({ field }) => (
                                 <FormItem>
                                     <FormLabel>Seu Número</FormLabel>
                                     <FormControl>
                                         <Input placeholder="Ex: (21) 123456789" {...field}/>
                                     </FormControl>
-                                    <FormDescription />
-                                    <FormMessage />
                                 </FormItem>
                             )}
                         />
-                        <button onSubmit={form.handleSubmit(OnSubmit)} className="mt-2 w-64 max-sm:w-40 bg-(--color-button-darkblue) sm:hover:w-68 text-white px-3 py-2 rounded-3xl">Enviar Mensagem</button>
+                        <button type="submit" className="mt-2 w-64 max-sm:w-40 bg-(--color-button-darkblue) sm:hover:w-68 text-white px-3 py-2 rounded-3xl">Enviar Mensagem</button>
                     </form>    
                 </Form>
             </CardContent>
