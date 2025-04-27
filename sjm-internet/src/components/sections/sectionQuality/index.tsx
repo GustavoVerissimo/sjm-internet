1a1729ee73746e95ad6adcbe4f442ce0fd7af9d3
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-04-27 13:05:25 -0300

    iniciando section map

--- sjm-internet/src/components/sections/sectionQuality/index.tsx
+++ sjm-internet/src/components/sections/sectionQuality/index.tsx
@@ -1,7 +1,7 @@
 export default function SectionQuality() {
     return(
         <section className="w-full h-[300px] bg-(--color-background-grey)">
-
+                
         </section>
     )
 }
