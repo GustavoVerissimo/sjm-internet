33660a7a0b6dfacafa3b2a42fbe9e7212a50b69a
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-05-08 18:14:47 -0300

    modificando section streaming

---
+++ sjm-internet/src/app/streamings/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header"
+import Footer from "@/components/footer"
+
+export default function Dispobilidade(){
+    return(
+        <>
+            <Header />
+            <div className="h-full bg-(--color-background-grey) justify-items-center">
+            </div>
+            <Footer />
+        </>
+    )
+}
