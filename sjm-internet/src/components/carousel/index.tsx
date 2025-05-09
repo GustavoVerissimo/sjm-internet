8bc51f7b8a74d8675eca444b5d89a93301981fe0
gustavoverissimo <gustavo.monteiro.6@gmail.com>
2025-05-09 18:52:38 -0300

    modificando section streaming

--- sjm-internet/src/components/carousel/index.tsx
+++ sjm-internet/src/components/carousel/index.tsx
@@ -1,4 +1,9 @@
+"use client"
+
 import * as React from "react"
+
+import Autoplay from "embla-carousel-autoplay"
+
 import imageMax450 from "../../../public/image/planos/max-450.jpg"
 import imageMax650 from "../../../public/image/planos/max-650.jpg"
 import imageMax800 from "../../../public/image/planos/max-800.jpg"
@@ -6,6 +11,8 @@ import imageParamount450 from "../../../public/image/planos/paramount-450.jpg"
 import imageParamount650 from "../../../public/image/planos/paramount-650.jpg"
 import imageParamount800 from "../../../public/image/planos/paramount-800.jpg"
 import Image from "next/image"
+
+import { type CarouselApi } from "@/components/ui/carousel"
 
 import {
   Carousel,
@@ -23,22 +30,59 @@ export function CarouselSectionPrice(){
         {id: 4 , image: imageParamount650},
         {id: 5 , image: imageMax800},
         {id:6 , image: imageParamount800},
-    ]
+    ];
+    const [api, setApi] = React.useState<CarouselApi>()
+  const [current, setCurrent] = React.useState(0)
+  const [count, setCount] = React.useState(0)
+ 
+  React.useEffect(() => {
+    if (!api) {
+      return
+    }
+ 
+    setCount(api.scrollSnapList().length)
+    setCurrent(api.selectedScrollSnap() + 1)
+ 
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1)
+    })
+  }, [api]);
+
   return (
-    <Carousel className=" w-full max-w-2xl">
-      <CarouselContent className="-ml-1">
-        {
-            data.map((data) => (
-                <CarouselItem key={data.id} className="pl-1 lg:basis-1/2 xl:basis-1/3">
-                    <div className="flex aspect-square items-center justify-center">
-                      <div className="items-center w-[450px] h-[300px] max-sm:w-[200px] max-sm:h-[300px] rounded-xl">
-                        <Image alt="Imagem que descreve o plano fibra 450 megas + streaming max" src={data.image} className="w-[100%] h-[100%] rounded-xl" />
-                      </div>
-                    </div>  
-                </CarouselItem>
-                ))
-        }
-      </CarouselContent>
-    </Carousel>
+    <div className="md:hidden">
+      <Carousel 
+        className=" w-full max-w-2xl"
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 3000,
+          }),
+        ]}
+        setApi={setApi}
+      >
+        <CarouselContent className="-ml-1">
+          {
+              data.map((data) => (
+                  <CarouselItem key={data.id} className="">
+                      <div className="flex aspect-square items-center justify-center">
+                        <div className="items-center w-[450px] h-[300px] max-sm:w-[200px] max-sm:h-[300px] rounded-xl">
+                          <Image alt="Imagem que descreve o plano fibra 450 megas + streaming max" src={data.image} className="w-[100%] h-[100%] rounded-xl" />
+                        </div>
+                      </div>  
+                  </CarouselItem>
+
+                  ))
+          }
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="py-2 text-center text-sm text-muted-foreground">
+        Slide {current} of {count}
+      </div>
+    </div>  
   )
 }
